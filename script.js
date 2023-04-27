200540c2bd6d38b9cff04109f4f869d2936d62f8
Arnab-Saha-2506 <89330067+Arnab-Saha-2506@users.noreply.github.com>
2023-04-27 12:37:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// console.log("Welcome to the web piccaso workshop!!")
+
+let nav = document.getElementById('mid');
+// console.log(nav)
+
+function submit(){
+    // alert("Form submitted")
+    // prompt("Form submitted")
+    // let name = document.getElementById('name')
+    // console.log(name.value)
+    // let lists = document.getElementById('points')
+    // console.log(lists.innerHTML)
+    let list = document.getElementsByClassName('list')
+    console.log(list[0].innerHTML)
+}
